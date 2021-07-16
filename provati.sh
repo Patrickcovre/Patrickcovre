4c0ddee4bd40ea2fc7e71e6ef0b08bb96dc61d8e
PatrickCovre <patrick_c_rodrigues@estudante.sc.senai.br>
2021-07-16 09:19:34 -0300

    provati2

---
+++ provati.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "nome do arquivo"
+read $1
+ls -l $1
+echo "O usuario dono tem permissao de ler, escrever e executar
+r=ler
+x=executar
+w=escrever
+
+
+#!bin/bash
+cat /etc/passwd
